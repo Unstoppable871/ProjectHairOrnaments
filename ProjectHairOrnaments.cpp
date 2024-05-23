ed87e9c7f35cdebfaa59f3309dc760d6bb5586d1
Unstoppable871 <147102629+Unstoppable871@users.noreply.github.com>
2024-05-23 15:08:19 +0400

    Add project files.

---
+++ ProjectHairOrnaments.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class HairOrnaments {
+    string name;
+    string colour;
+    double price;
+};
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
