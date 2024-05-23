93c47fe142872623b68aea28a9c62ed5bf8c648d
Unstoppable871 <147102629+Unstoppable871@users.noreply.github.com>
2024-05-23 15:15:52 +0400

    Getters

--- ProjectHairOrnaments.cpp
+++ ProjectHairOrnaments.cpp
@@ -10,6 +10,18 @@ class HairOrnaments {
 public:
     HairOrnaments(const string& n, const string& c,double p):
         name(n),colour(c),price(p){}
+
+    string getName() {
+        return name;
+    }
+
+    string getColour() {
+        return colour;
+    }
+
+    double getPrice() {
+        return price;
+    }
        
 };
 
