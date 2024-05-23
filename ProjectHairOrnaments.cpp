b632b4f52eac46908b4d629bee3e8cba1355bb4c
Unstoppable871 <147102629+Unstoppable871@users.noreply.github.com>
2024-05-23 15:13:12 +0400

    Constructor of HairOrnaments

--- ProjectHairOrnaments.cpp
+++ ProjectHairOrnaments.cpp
@@ -6,6 +6,11 @@ class HairOrnaments {
     string name;
     string colour;
     double price;
+
+public:
+    HairOrnaments(const string& n, const string& c,double p):
+        name(n),colour(c),price(p){}
+       
 };
 
 int main()
